c0bd5a5fc5489b071d9e7a1660aec231b6224d4f
StephensGit <stephenmoran@live.ie>
2021-10-20 18:18:45 +0100

    React Router added

--- README.md
+++ README.md
@@ -13,3 +13,15 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
 The page will reload if you make edits.\
 You will also see any lint errors in the console.
+
+# React Router Dom
+
+##npm i react-router-dom
+
+# Axios
+
+##npm i axios
+
+# Links
+
+Using links instead of a tags
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = (props) => {
   return (
     <div>
       <h1>{props.title}</h1>
+      <ul>
+        <li>
+          <Link to='about'>about</Link>
+        </li>
+        <li>
+          <Link to='/'>home</Link>
+        </li>
+      </ul>
     </div>
   );
 };
